9b36e69e37bf3556e808287a304f7b3e8caec6f6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-10 15:17:14 +0300

    Update contacts-selectors.js

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,24 +1,3 @@
-import { toast } from 'react-toastify';
-import { toastifyOptions } from 'utils/toastifyOptions';
-
 export const getContacts = state => state.contacts.items;
-
-export const getFilteredContacts = ({ filter, contacts: { items } }) => {
-  if (!filter) {
-    return items;
-  }
-  const normalizedFilter = filter.toLowerCase();
-  const filteredContacts = items.filter(
-    ({ name, phone }) =>
-      name.toLowerCase().trim().includes(normalizedFilter) ||
-      phone.trim().includes(normalizedFilter)
-  );
-
-  if (normalizedFilter && !filteredContacts.length) {
-    toast.warn(`No contacts matching your request`, toastifyOptions);
-  }
-  return filteredContacts;
-};
-
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
