7e8f84a9656e1a75c7545b91b9558497dae72e8a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-10 15:18:13 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import contactsReducer from './contacts/contacts-slice';
 import filterReducer from './filter/filter-slice';
 
-// об'єкт налаштувань, в якому записані, які дані зберігати в Local Storage
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-// дані з локал сторіджа потрапляли відразу в redux під час завантаження
-export const persistor = persistStore(store);
