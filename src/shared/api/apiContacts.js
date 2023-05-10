d313508c0065b18ced2d032c1a8e2b1efa60e294
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-10 03:15:43 +0300

    Create apiContacts.js

---
+++ src/shared/api/apiContacts.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const contactsInstance = axios.create({
+  baseURL: 'https://64302ad0b289b1dec4c2d198.mockapi.io/contacts',
+});
+
+export const getAllContacts = () => contactsInstance.get('/');
+
+export const deleteContact = id => {
+  return contactsInstance.delete(`/${id}`);
+};
+
+export const addContact = data => {
+  return contactsInstance.post('/', data);
+};
