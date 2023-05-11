69ca7373f6722b8d08758df81d04c75fc94896cb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-11 13:37:26 +0300

    Create modalStyles.js

---
+++ src/styles/modalStyles.js
@@ -0,0 +1,44 @@
+export const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(142,154,175, 0.3)',
+    zIndex: 1300,
+  },
+  content: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    zIndex: '1200',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '100%',
+    width: '420px',
+    height: 'auto',
+    boxShadow: '2px 2px 2px #0f0f0f',
+    border: 'none',
+    borderRadius: 'none',
+  },
+};
+export const customStylesInsideModal = {
+  overlay: {
+    backgroundColor: 'rgba(142,154,175, 0.3)',
+    zIndex: 1301,
+  },
+  content: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    zIndex: '1201',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '100%',
+    width: '420px',
+    height: 'auto',
+    boxShadow: '2px 2px 2px #0f0f0f',
+    border: 'none',
+    borderRadius: 'none',
+  },
+};
