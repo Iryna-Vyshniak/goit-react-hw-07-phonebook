b8b9266ccf6aca3974e53f79b9a1fbd384c1ead2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-11 13:47:42 +0300

    Create ChangeContactModal.jsx

---
+++ src/components/ChangeContactModal/ChangeContactModal.jsx
@@ -0,0 +1,120 @@
+import { useState } from 'react';
+import { Formik } from 'formik';
+import 'yup-phone';
+
+// redux
+import { BsFillTelephoneFill, BsPersonFill } from 'react-icons/bs';
+import { IoMdPersonAdd } from 'react-icons/io';
+import { schema } from 'shared/schemaYup';
+import {
+  Form,
+  FormField,
+  FieldFormik,
+  ErrorMessage,
+  StyledButton,
+  LabelWrapper,
+  LabelSpan,
+} from './ChangeContactModal.styled';
+
+import Modal from 'react-modal';
+
+import { MdOutlineClose } from 'react-icons/md';
+import { CloseBtn } from 'components/Modal/Modal.styled';
+import { useDispatch } from 'react-redux';
+import { changeContact } from 'redux/contacts/contacts-operations';
+
+import { customStylesInsideModal } from 'styles/modalStyles';
+
+Modal.setAppElement('#root');
+
+export const ChangeContactModal = ({ isOpen, data, onClose }) => {
+  const [formValues, setFormValues] = useState(data || {});
+
+  console.log(data);
+  //const { name, phone, avatar } = formValues; //data from list item, where click on 'edit' button
+
+  const initialValues = { avatar: '', name: '', phone: '' };
+  const savedValues = {
+    avatar: data?.avatar || '',
+    name: data?.name || '',
+    phone: data?.phone || '',
+  };
+  console.log(formValues);
+
+  const dispatch = useDispatch();
+
+  const onChangeContact = e => {
+    setFormValues(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    dispatch(changeContact(formValues));
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        contentLabel="Inline Styles Modal Example"
+        style={customStylesInsideModal}
+      >
+        <CloseBtn onClick={onClose}>
+          <MdOutlineClose />
+        </CloseBtn>
+        <Formik
+          initialValues={savedValues || initialValues}
+          onSubmit={(values, { resetForm }) => {
+            onChangeContact({ ...values });
+            resetForm();
+          }}
+          enableReinitialize
+          validationSchema={schema}
+        >
+          {formik => {
+            // console.log('Formik props', formik);
+            return (
+              <Form autoComplete="off">
+                <FormField>
+                  <LabelWrapper>
+                    <BsPersonFill />
+                    <LabelSpan>Avatar</LabelSpan>
+                  </LabelWrapper>
+                  <FieldFormik name="avatar" placeholder="Add link to avatar" />
+                  <ErrorMessage name="avatar" component="span" />
+                </FormField>
+                <FormField>
+                  <LabelWrapper>
+                    <BsPersonFill />
+                    <LabelSpan>Name</LabelSpan>
+                  </LabelWrapper>
+                  <FieldFormik type="text" name="name" placeholder="Name" />
+                  <ErrorMessage name="name" component="span" />
+                </FormField>
+                <FormField>
+                  <LabelWrapper>
+                    <BsFillTelephoneFill />
+                    <LabelSpan>Number</LabelSpan>
+                  </LabelWrapper>
+                  <FieldFormik
+                    type="tel"
+                    name="phone"
+                    placeholder="+38-050-123-45-67"
+                  />
+                  <ErrorMessage name="phone" component="span" />
+                </FormField>
+                <StyledButton
+                  type="submit"
+                  disabled={!formik.isValid || formik.isSubmitting}
+                >
+                  <IoMdPersonAdd size="16" />
+                  Edit contact
+                </StyledButton>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Modal>
+    </>
+  );
+};
